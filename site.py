fdf1bbbf579c830d2a1d901df6f6c1444ea52188
moloch <dario.coco@gmail.com>
2015-06-18 15:54:19 +0200

    Flask templates

--- site.py
+++ site.py
@@ -3,12 +3,24 @@ app = Flask(__name__,'')
 app.debug = True
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/mics')
+def mics():
+    return render_template('mics.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
 
 if __name__ == '__main__':
     app.run()
